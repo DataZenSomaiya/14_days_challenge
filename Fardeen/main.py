2e821edd79739110d527dac24974e5cd50104199
Fardeen Khan <fardeen.mk@somaiya.edu>
2022-09-06 20:43:25 +0530

    defining car class

--- Fardeen/main.py
+++ Fardeen/main.py
@@ -9,6 +9,19 @@ SCREEN  = pygame.display.set_mode((screen_width, screen_height))
 
 TRACK = pygame.image.load(os.path.join("assets", "track.png"))
 
+#car class for defining car object
+class Car(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.original_image = pygame.image.load(os.path.join("assets", "car.png"))
+        self.image = self.original_image
+        self.rect = self.image.get_rect(center = (490, 820))
+        self.drive_state = False
+        self.vel_vector = pygame.math.Vcetor2(0.8, 0)
+        self.angle = 0
+
+
+#main function
 def eval_genomes():
     run = True
     while run: 
