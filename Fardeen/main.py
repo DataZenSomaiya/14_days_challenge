2d8e714abcbf0653ae451b091f5018b4f45134c4
Fardeen Khan <fardeen.mk@somaiya.edu>
2022-09-09 18:32:18 +0530

    fixed some bugs and added movement

--- Fardeen/main.py
+++ Fardeen/main.py
@@ -17,13 +17,15 @@ class Car(pygame.sprite.Sprite):
         self.image = self.original_image
         self.rect = self.image.get_rect(center = (490, 820))
         self.drive_state = False
-        self.vel_vector = pygame.math.Vcetor2(0.8, 0)
+        self.vel_vector = pygame.math.Vector2(0.8, 0)
         self.angle = 0
+        self.rotation_vel = 0
+        self.direction = 0
 
     #updates the position and angle every time called 
     def update(self):
         self.drive()
-        self.update()
+        self.rotate()
 
     #changes the pos of car accordingly
     def drive(self):
@@ -31,8 +33,15 @@ class Car(pygame.sprite.Sprite):
             self.rect.center += self.vel_vector * 6
 
     def rotate(self):
+        if self.direction == 1:
+            self.angle -= self.rotation_vel
+            self.vel_vector.rotate_ip(self.rotation_vel)
+        if self.direction == -1:
+            self.angle += self.rotation_vel
+            self.vel_vector.rotate_ip(-self.rotation_vel)
+        
         self.image = pygame.transform.rotozoom(self.original_image, self.angle, 0.1)
-
+        self.rect = self.image.get_rect(center=self.rect.center)
 
 car = pygame.sprite.GroupSingle(Car())
 
@@ -49,12 +58,18 @@ def eval_genomes():
 
         #taking input from the user 
         user_input = pygame.key.get_pressed()
-        if sum(pygame.key.get_pressed) <= 1:
+        if sum(pygame.key.get_pressed()) <= 1:
             car.sprite.drive_state = False
 
         #drive
         if user_input[pygame.K_UP]:
             car.sprite.drive_state= True
+
+        #update
+        car.draw(SCREEN)
+        car.update()
+        pygame.display.update()
+
 
 eval_genomes()
         
