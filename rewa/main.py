f35df306ba92dda222abf78971691331075f6723
Rewa Dambal <rewadambal@gmail.com>
2022-09-11 16:28:41 +0530

    Day Six : First Commit

--- rewa/main.py
+++ rewa/main.py
@@ -1,12 +1,18 @@
 from bs4 import BeautifulSoup
 import requests
-response = requests.get('https://en.wikipedia.org/wiki/India')
-print(response.status_code)
-src = response.content
-soup = BeautifulSoup(src , 'lxml')
-links = soup.find_all("a")
-print(links)
-for link in links :
-    if "languages" in link.text :
-        print(link)
-        print(link.attrs['href'])
+html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
+soup = BeautifulSoup(html_text , 'lxml')
+jobs = soup.find_all('li', class_ = 'clearfix job-bx wht-shd-bx')
+for job in jobs:
+    comp_name = job.find('h3', class_ = 'joblist-comp-name').text.replace(' ','')
+    skills = job.find('span', class_ = 'srp-skills').text.replace(' ','')
+    date_post = job.find('span', class_ = 'sim-posted').span.text
+    print(f'''
+    Company Name : {comp_name}
+    Skills Required : {skills}
+    Date When Posted : {date_post}
+    ''')
+
+    print('')
+
+
