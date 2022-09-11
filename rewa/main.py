cecd1f0851e2bd23bc5b74a2695359f3710f1531
Rewa Dambal <rewadambal@gmail.com>
2022-09-11 22:12:40 +0530

    Day Seven : Second Commit

--- rewa/main.py
+++ rewa/main.py
@@ -1,5 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
+
+print('Put some skill that you are not familiar with')
+unfamiliar_skill = input('>')
+print(f'Filtering Out {unfamiliar_skill}')
+
 html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
 soup = BeautifulSoup(html_text , 'lxml')
 jobs = soup.find_all('li', class_ = 'clearfix job-bx wht-shd-bx')
@@ -9,10 +14,11 @@ for job in jobs:
         comp_name = job.find('h3', class_ = 'joblist-comp-name').text.replace(' ','')
         skills = job.find('span', class_ = 'srp-skills').text.replace(' ','')
         more_info = job.header.h2.a['href']
-        print(f'Company Name : {comp_name.strip()}')
-        print(f'Required Skills : {skills.strip()}')
-        print(f'Published Date : {date_post}')
-        print(f'More Information : {more_info}')
+        if unfamiliar_skill not in skills:
+            print(f'Company Name : {comp_name.strip()}')
+            print(f'Required Skills : {skills.strip()}')
+            print(f'Published Date : {date_post}')
+            print(f'More Information : {more_info}')
 
 
         print('')
