62648ec6924f3174b2cf7b149e08118a78160a8e
Rewa Dambal <rewadambal@gmail.com>
2022-09-11 22:04:56 +0530

    Day Seven : First Commit

--- rewa/main.py
+++ rewa/main.py
@@ -4,15 +4,15 @@ html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?se
 soup = BeautifulSoup(html_text , 'lxml')
 jobs = soup.find_all('li', class_ = 'clearfix job-bx wht-shd-bx')
 for job in jobs:
-    comp_name = job.find('h3', class_ = 'joblist-comp-name').text.replace(' ','')
-    skills = job.find('span', class_ = 'srp-skills').text.replace(' ','')
-    date_post = job.find('span', class_ = 'sim-posted').span.text
-    print(f'''
-    Company Name : {comp_name}
-    Skills Required : {skills}
-    Date When Posted : {date_post}
-    ''')
+    date_post = job.find('span', class_='sim-posted').span.text
+    if 'few' in date_post:
+        comp_name = job.find('h3', class_ = 'joblist-comp-name').text.replace(' ','')
+        skills = job.find('span', class_ = 'srp-skills').text.replace(' ','')
+        more_info = job.header.h2.a['href']
+        print(f'Company Name : {comp_name.strip()}')
+        print(f'Required Skills : {skills.strip()}')
+        print(f'Published Date : {date_post}')
+        print(f'More Information : {more_info}')
 
-    print('')
 
-
+        print('')
