8937e7d9dbd7b8d1a4cfe94459e8ddee9d556d2d
Rewa Dambal <rewadambal@gmail.com>
2022-09-12 23:02:41 +0530

    Day Eight : First Commit

--- rewa/main.py
+++ rewa/main.py
@@ -1,24 +1,20 @@
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
 
-print('Put some skill that you are not familiar with')
-unfamiliar_skill = input('>')
-print(f'Filtering Out {unfamiliar_skill}')
-
-html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
-soup = BeautifulSoup(html_text , 'lxml')
-jobs = soup.find_all('li', class_ = 'clearfix job-bx wht-shd-bx')
+result = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=java&txtLocation=').text
+soup = BeautifulSoup(result,'lxml')
+jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
 for job in jobs:
-    date_post = job.find('span', class_='sim-posted').span.text
-    if 'few' in date_post:
-        comp_name = job.find('h3', class_ = 'joblist-comp-name').text.replace(' ','')
-        skills = job.find('span', class_ = 'srp-skills').text.replace(' ','')
-        more_info = job.header.h2.a['href']
-        if unfamiliar_skill not in skills:
-            print(f'Company Name : {comp_name.strip()}')
-            print(f'Required Skills : {skills.strip()}')
-            print(f'Published Date : {date_post}')
-            print(f'More Information : {more_info}')
+    comp_name = job.find('h3', class_ = 'joblist-comp-name').text.replace(' ','')
+    skills = job.find('span', class_ = 'srp-skills').text.replace(' ','')
+    update_post = job.find('span',class_ = 'sim-posted').span.text
+
+    print(f'Company Name : {comp_name.strip()}')
+    print(f'Skills Required : {skills.strip()}')
+    print(f'Post Updated on : {update_post}')
+
+print('')
 
 
-        print('')
+
+
