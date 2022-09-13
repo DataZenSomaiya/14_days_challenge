9c62ed51ce8b30ad37a65665945eb9d5891ab54f
Rewa Dambal <rewadambal@gmail.com>
2022-09-13 22:00:40 +0530

    Day Nine : First Commit

---
+++ rewa/main_2.py
@@ -0,0 +1,18 @@
+#Input
+name = input("What is your name? ")
+print("Hello "+ name)
+print(' ')
+
+#Type Conversion
+birth_yr = input("Enter your birth year ")
+age = 2022 - int(birth_yr)
+print(f'Your age is : {age}')
+print(' ')
+
+#Strings
+course = 'Python for Beginners'
+print(course)
+print(course.find('r'))
+print(course.replace('n','4'))
+print('Python' in course)
+print(' ')
