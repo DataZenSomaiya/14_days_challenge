a75b7bdf783577e6717535bc1cce03c4a42f5e1e
Rewa Dambal <rewadambal@gmail.com>
2022-09-14 22:25:54 +0530

    Day Ten : First Commit

--- rewa/main_2.py
+++ rewa/main_2.py
@@ -16,3 +16,26 @@ print(course.find('r'))
 print(course.replace('n','4'))
 print('Python' in course)
 print(' ')
+
+#Arithmetic Operators
+print(10+3)
+print(10-3)
+print(10*3)
+print(10/3)
+print(10//3)
+print(10%3)
+x = 10
+x = x + 3
+print(x)
+print(' ')
+
+#Exercise
+weight = input("Weight : ")
+unit = input("(K)g or (L)bs : ")
+unit.upper()
+if unit == "K":
+    conv = float(weight)/0.45
+    print(f'Weight in Pounds : {conv}')
+else:
+    conv = float(weight) * 0.45
+    print(f'Weight in Kilogram : {conv}')
