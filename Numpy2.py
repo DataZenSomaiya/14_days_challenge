a4e3e2ba29f5b4c8bfa7d390afa35bb544a03b20
Dipesh <dipesh.khichada@somaiya.edu>
2022-09-14 15:52:55 +0530

    Mini Projects

---
+++ Numpy2.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# All 0s matrix
+print(np.zeros((2,3)))
+print('*'*25)
+# All 1s matrix
+print(np.ones((4,2,2), dtype='int32'))
+print('*'*25)
+
+# Any other number
+print(np.full((2,3,2), 99, dtype='int32'))
+print('*'*25)
+
