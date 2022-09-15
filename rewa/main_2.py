69586ed98faf7d06fb1276cb35dfc00be0b06ad4
Rewa Dambal <rewadambal@gmail.com>
2022-09-15 22:34:47 +0530

    Day Eleven : First Commit

--- rewa/main_2.py
+++ rewa/main_2.py
@@ -39,3 +39,32 @@ if unit == "K":
 else:
     conv = float(weight) * 0.45
     print(f'Weight in Kilogram : {conv}')
+
+#While loop
+i = 1
+while i<=5:
+    print(i)
+    i = i+1
+j = 1
+while j<=10:
+    print(j * '*')
+    j = j+1
+
+#Lists
+names = ["John", "Bob", "Rewa", "Sam"]
+print(names)
+names[0] = "Jon"
+print(names[0:3])
+
+#Objects
+numbers = [1,2,3,4,5]
+numbers.append(6)
+print(numbers)
+numbers.insert(0,-1)
+print(numbers)
+numbers.remove(3)
+print(numbers)
+print(1 in numbers)
+print(len(numbers))
+numbers.clear()
+print(numbers)
