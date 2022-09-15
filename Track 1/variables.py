8a1b5259db676a5861af17f35f5479c428134a07
divyagala1135 <112709269+divyagala1135@users.noreply.github.com>
2022-09-15 09:50:20 +0530

    Python in 1 hour

---
+++ Track 1/variables.py
@@ -0,0 +1,10 @@
+from gettext import GNUTranslations
+name = "Divay Galani"
+age = 17
+std = "FY BTECH EXTC"
+college = "K J SOMAIYA COLLEGE OF ENGINEERING"
+print(name)
+print(age)
+print(std)
+print(college)
+
